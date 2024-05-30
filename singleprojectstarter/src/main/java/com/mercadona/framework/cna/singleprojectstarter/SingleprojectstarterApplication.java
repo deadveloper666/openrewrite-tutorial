5aa3a2f0933d5cd6ea187827cae173aa31b7d039
Daniel Peña Perez <danipenaperez@gmail.com>
2024-05-30 11:49:36 +0200

    Initial import

---
+++ singleprojectstarter/src/main/java/com/mercadona/framework/cna/singleprojectstarter/SingleprojectstarterApplication.java
@@ -0,0 +1,13 @@
+package com.mercadona.framework.cna.singleprojectstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleprojectstarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleprojectstarterApplication.class, args);
+	}
+
+}
---
+++ singleprojectstarter/src/test/java/com/mercadona/framework/cna/singleprojectstarter/SingleprojectstarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadona.framework.cna.singleprojectstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleprojectstarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
